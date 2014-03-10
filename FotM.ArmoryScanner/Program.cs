4556a61a971f185d90093886e1c37f67ef0d6d64
Tagir Magomedov <tagir.magomedov@gmail.com>
2014-03-10 01:13:41 -0700

    Added Logging via log4net. Downloaded json puller and downloaded armory results.

--- FotM.ArmoryScanner/Program.cs
+++ FotM.ArmoryScanner/Program.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FotM.Utilities;
+using log4net;
+using log4net.Config;
 
 namespace FotM.ArmoryScanner
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly ILog Logger = LoggingExtensions.GetLogger<Program>();
+
+        static void Main()
         {
+            XmlConfigurator.Configure();
+
+            Logger.Debug("App started");
+
+            var rawArmoryPuller = new RawJsonPuller("http://us.battle.net/api/wow/leaderboard/");
+
+            var result = rawArmoryPuller.DownloadJson<dynamic>("3v3").rows;
+            string str = result[0].name;
+            Logger.Info(str);
         }
     }
 
-    public class RawPuller
-    {
-        
-    }
-
-    public class DataPuller
-    {
-        // http://blizzard.github.io/api-wow-docs/#pvp-api/leaderboard-api
-
-    }
+    // http://blizzard.github.io/api-wow-docs/#pvp-api/leaderboard-api
 }
---
+++ FotM.Utilities/LoggingExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace FotM.Utilities
+{
+    public static class LoggingExtensions
+    {
+        public static ILog GetLogger<T>()
+        {
+            return LogManager.GetLogger(typeof (T));
+        }
+    }
+}
