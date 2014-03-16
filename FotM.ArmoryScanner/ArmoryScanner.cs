b1cf003239852dd1f2f95f4aa175b55e7859ec7d
Tagir Magomedov <tagir.magomedov@gmail.com>
2014-03-15 18:25:49 -0700

    Moved TeamStats to Domain.

--- FotM.ArmoryScanner/ArmoryScanner.cs
+++ FotM.ArmoryScanner/ArmoryScanner.cs
@@ -167,39 +167,4 @@ namespace FotM.ArmoryScanner
                 : (double?) null;
         }
     }
-
-    public class TeamStats
-    {
-        public TeamStats()
-        {
-        }
-
-        public TeamStats(Team team)
-        {
-            RatingChange = 0;
-            TimesSeen = 1;
-            Team = team;
-            Seen = DateTime.Now;
-        }
-
-        public double RatingChange { get; set; }
-        public int TimesSeen { get; set; }
-        public DateTime Seen { get; set; }
-        public Team Team { get; set; }
-
-        public bool IsVerified
-        {
-            get
-            {
-                return TimesSeen >= 2; // same setup seen twice or more
-            }
-        }
-
-        public void Update(double ratingChange)
-        {
-            RatingChange += ratingChange;
-            Seen = DateTime.Now;
-            ++TimesSeen;
-        }
-    }
 }
---
+++ FotM.Domain/TeamStats.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FotM.Domain
+{
+    public class TeamStats
+    {
+        public TeamStats()
+        {
+        }
+
+        public TeamStats(Team team)
+        {
+            RatingChange = 0;
+            TimesSeen = 1;
+            Team = team;
+            Seen = DateTime.Now;
+        }
+
+        public double RatingChange { get; set; }
+        public int TimesSeen { get; set; }
+        public DateTime Seen { get; set; }
+        public Team Team { get; set; }
+
+        public bool IsVerified
+        {
+            get
+            {
+                return TimesSeen >= 2; // same setup seen twice or more
+            }
+        }
+
+        public void Update(double ratingChange)
+        {
+            RatingChange += ratingChange;
+            Seen = DateTime.Now;
+            ++TimesSeen;
+        }
+    }
+}
