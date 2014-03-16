70d15a9ed9e99106c3e7ca04eca74bb10aed8612
Tagir Magomedov <tagir.magomedov@gmail.com>
2014-03-15 21:26:17 -0700

    Added tests for CompressionUtils

---
+++ FotM.Utilities.Tests/CompressionTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FotM.Utilities.Tests
+{
+    [TestFixture]
+    public class CompressionTests
+    {
+        [Test]
+        public void UnzippedStringShouldBeTheSameEn()
+        {
+            const string str = "Hello, world!";
+
+            var zipped = CompressionUtils.Zip(str);
+            string unzipped = CompressionUtils.Unzip(zipped);
+
+            Assert.AreEqual(str, unzipped);
+        }
+
+        [Test]
+        public void UnzippedStringShouldBeTheSameRu()
+        {
+            const string str = "Привет, мир!";
+
+            var zipped = CompressionUtils.Zip(str);
+            string unzipped = CompressionUtils.Unzip(zipped);
+
+            Assert.AreEqual(str, unzipped);
+        }
+    }
+}
