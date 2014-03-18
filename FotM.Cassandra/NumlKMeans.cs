0e37e823e63c51eedee7a6a5997780031741b61c
Tagir Magomedov <tagir.magomedov@gmail.com>
2014-03-17 21:21:48 -0700

    Removed realm from feature set for Cassandra. Hamming prediction accuracy fell below 1%!

--- FotM.Cassandra/NumlKMeans.cs
+++ FotM.Cassandra/NumlKMeans.cs
@@ -7,9 +7,11 @@ namespace FotM.Cassandra
 {
     public class NumlKMeans: IKMeans<PlayerDiff>
     {
+        private readonly IDistance _distanceMetric;
+
         private static readonly Descriptor DiffDescriptor = Descriptor
             .For<PlayerDiff>()
-            .With(d => d.RealmId)
+            //.With(d => d.RealmId)
             .With(d => d.Ranking)
             .With(d => d.Rating)
             .With(d => d.WeeklyWins)
@@ -23,13 +25,18 @@ namespace FotM.Cassandra
             .With(d => d.SeasonLossesDiff)
             .With(d => d.RatingDiff);
 
+        public NumlKMeans(IDistance distanceMetric = null)
+        {
+            _distanceMetric = distanceMetric ?? new HammingDistance();
+        }
+
         public int[] Compute(PlayerDiff[] diffs, int nGroups)
         {
             var kMeans = new KMeans();
             kMeans.Descriptor = DiffDescriptor;
 
             // I have no idea why, but HammingDistance brings prediction accuracy from 33% to whooping 90%
-            return kMeans.Generate(diffs, nGroups, new HammingDistance());
+            return kMeans.Generate(diffs, nGroups, _distanceMetric);
         }
     }
 }
