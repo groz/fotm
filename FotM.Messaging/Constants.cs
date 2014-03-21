d00fa38ac7c582e81643d05c6f4f52aa61eda87f
Tagir Magomedov <tagir.magomedov@gmail.com>
2014-03-20 22:26:06 -0700

    Added config project.

--- FotM.Messaging/Constants.cs
+++ FotM.Messaging/Constants.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FotM.Config;
 using FotM.Utilities;
 using System.Configuration;
 
@@ -12,16 +13,7 @@ namespace FotM.Messaging
     {
         static Constants()
         {
-            string key = "Microsoft.ServiceBus.ConnectionString";
-            ConnectionString = ConfigurationManager.AppSettings[key];
-
-            // only load library specific config if the key is not found in current AppSettings
-            if (ConnectionString == null)
-            {
-                var cfg = ConfigHelpers.LoadConfig();
-                var appSettings = cfg.AppSettings.Settings;
-                ConnectionString = appSettings[key].Value;
-            }
+            ConnectionString = RegionalConfig.Instance.ServiceBusConnectionString;
         }
 
         internal static readonly string ConnectionString;
