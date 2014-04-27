f15043a27b9bfc781f5560e00c9b1a5357709575
Tagir Magomedov <tagir.magomedov@gmail.com>
2014-04-27 04:33:08 -0700

    Implemented math portion of kmeans in F#

--- FotM.Athena/Math.fs
+++ FotM.Athena/Math.fs
@@ -1,5 +1,19 @@
 ﻿namespace FotM.Athena
 
+// extensions
+
+module Seq =
+    let mini s = s |> Seq.mapi (fun i x -> (i, x)) |> Seq.minBy snd
+    let miniBy f s = s |> Seq.mapi (fun i x -> (i, f(x))) |> Seq.minBy snd
+
+module List =
+    let mini s = s |> List.mapi (fun i x -> (i, x)) |> List.minBy snd
+    let miniBy f s = s |> List.mapi (fun i x -> (i, f(x))) |> List.minBy snd
+
+module Array =
+    let mini s = s |> Array.mapi (fun i x -> (i, x)) |> Array.minBy snd
+    let miniBy f s = s |> Array.mapi (fun i x -> (i, f(x))) |> Array.minBy snd
+
 module Math =
     let n_choose_k n k = 
         let rec choose lo  =
@@ -20,6 +34,22 @@ module Math =
         squaredEuclideanDistance a b |> sqrt
 
     type Vector = float[]
+
+    module VectorOps = 
+        let (+) (left: Vector) (right: Vector) : Vector =
+            Array.zip left right |> Array.map (fun (l, r) -> l + r)
+
+        let (*) (v: Vector) (scale: float) : Vector = v |> Array.map ((*) scale)
+
+        let (/) (v: Vector) (scale: float) : Vector = v * (1.0/scale)
+
+        let zeroVector n : Vector = [|for i in 1..n do yield 0.0|]
+
+        let mean (n: int) (vectors: Vector array) =
+            if vectors.Length <> 0 then
+                ( vectors |> Array.reduce (fun acc v -> acc + v) ) / float(vectors.Length)
+            else
+                zeroVector(n)
         
     type AthenaKMeans<'a>(distance: float[] -> float[] -> float) =
 
@@ -32,40 +62,50 @@ module Math =
 
         let rng = System.Random()
 
-        let ``kmeans++`` matrix k m n =
-            
+        let ``kmeans++`` matrix k =            
             let rec buildCentroids(centroids: float[] list)(i: int)=
                 if i < k then 
                     let distanceToClosestCentroid(v: Vector): float = centroids |> List.map (squaredEuclideanDistance v) |> List.min
 
-                    let dx2 = matrix |> Array.map distanceToClosestCentroid
+                    let distances = matrix |> Array.map distanceToClosestCentroid
 
-                    let border = rng.NextDouble() * Array.sum(dx2)
+                    let border = rng.NextDouble() * Array.sum(distances)
 
-                    let rec nextCentroid runningSum idx =
-                        if runningSum < border then nextCentroid (runningSum+dx2.[idx]) (idx+1)
-                        else matrix.[idx-1]
+                    let rec nextCentroid runningSum i =
+                        if runningSum < border then nextCentroid (runningSum + distances.[i]) (i+1)
+                        else matrix.[i-1]
                 
                     let next = nextCentroid 0.0 0
 
                     buildCentroids (next::centroids) (i+1)
                 else
                     centroids
-            
-            buildCentroids [matrix.[rng.Next(m)]] 0
-                
-        let compute(matrix: float[][])(k: int): int[] =
+                                
+            buildCentroids [matrix.[rng.Next(matrix.Length)]] 0
 
-            let m = matrix.Length
+        let getPointsForCluster (clustering: int[]) (i: int) (matrix: Vector[])  =
+            clustering
+            |> Array.mapi (fun i ci -> i, ci)
+            |> Array.filter (fun idx -> snd idx = i)
+            |> Array.map (fun idx -> matrix.[fst idx])
+
+        let cluster(matrix: float[][])(k: int): int[] =
+
             let n = matrix.[0].Length
-            let centroids = ``kmeans++`` matrix k m n
+            let centroids = ``kmeans++`` matrix k
 
-            let rec iterate result =
+            let rec iterate (centroids: Vector list) (currentClustering: int[]) =
                 // I. assignment step
-                // II. update step
-                0
+                let newClustering = matrix |> Array.map (fun input -> fst( centroids |> List.miniBy(squaredEuclideanDistance input) ) )
 
-            [||]
+                if newClustering <> currentClustering then
+                    // II. update step
+                    let newCentroids = centroids |> List.mapi (fun i c -> matrix |> getPointsForCluster newClustering i |> VectorOps.mean n)
+                    iterate newCentroids newClustering
+                else
+                    currentClustering
+
+            iterate centroids [||]
 
         interface FotM.Utilities.IKMeans<'a> with
             member this.ComputeGroups(dataSet, nGroups) =
