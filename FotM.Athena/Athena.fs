cf5c2f717d6de48009d450b20b841b519b33f2d5
Tagir Magomedov <tagir.magomedov@gmail.com>
2014-05-06 00:58:33 -0700

    Added agents listening to Athena updates

--- FotM.Athena/Athena.fs
+++ FotM.Athena/Athena.fs
@@ -1,11 +1,18 @@
 ﻿namespace FotM.Athena
 
+open System
+open System.Net
 open FotM.Data
 open FotM.Hephaestus.TraceLogging
 open Math
 open FotM.Aether
 open Microsoft.ServiceBus.Messaging
 open System.Threading
+open Newtonsoft.Json
+
+type UpdateAgentMessage =
+| Update of Uri
+| Stop
 
 module Athena =
     (*
@@ -54,7 +61,7 @@ module Athena =
         // TODO: implement this
         []
     
-    let processUpdate(ladderSnapshot: LadderSnapshot, history: LadderSnapshot list) =
+    let processUpdate ladderSnapshot history =
         match history with
         | [] -> []
         | head :: tail ->
@@ -63,14 +70,60 @@ module Athena =
             let teams = groups |> List.collect findTeams
             teams
 
+    let updateProcessor region bracket = MailboxProcessor<UpdateAgentMessage>.Start(fun agent ->
+        logInfo "UpdateProcessor for %s, %s started" region.code bracket.url
+
+        let snapshotRepo = SnapshotRepository(region, bracket)
+        let teamRepo = SnapshotRepository(region, bracket)
+        
+        let rec loop history = async {
+            // TODO: filter out expired entries from history
+            let! updateMsg = agent.Receive()
+
+            match updateMsg with
+            | Update(storageLocation) ->
+                try
+                    use webClient = new WebClient()
+                    logInfo "fetching laddersnapshot from %A" storageLocation
+                    let! snapshotJson =  webClient.AsyncDownloadString storageLocation
+                    let snapshot = JsonConvert.DeserializeObject<LadderSnapshot> snapshotJson
+                    let teams = processUpdate snapshot history
+                    return! loop (snapshot::history)
+                with
+                | ex -> 
+                    logError "Exception while handling message for %s, %s: %A" region.code bracket.url ex
+                    return! loop history
+            | Stop ->
+                logInfo "UpdateProcessor for %s, %s stopped." region.code bracket.url
+        }
+
+        loop []
+    )
+
     let watch (updateTopic: SubscriptionClient) (waitHandle: WaitHandle) = async {
         logInfo "FotM.Athena entry point called, starting listening to armory updates..."
 
+        // creating processor agents
+        let processors = 
+            [
+                for region in Regions.all do
+                for bracket in Brackets.all do
+                yield (region, bracket), updateProcessor region bracket
+            ]
+            |> Map.ofList
+
+        // subscribing to messages
         updateTopic.OnMessage(fun msg->
-            logInfo "************ Message received: %A ***********" msg
+            logInfo "************ UpdateMessage received: %A ***********" msg
             let body = msg.GetBody<UpdateMesage>()
-            logInfo "************ Message handled: %A ***********" body
+
+            let processor = processors.[body.region, body.bracket]
+            processor.Post (Update body.storageLocation)
         )
 
+        // waiting until service requests shutdown
         waitHandle.WaitOne() |> ignore
+
+        // stopping processor agents
+        for p in processors do p.Value.Post Stop
     }
