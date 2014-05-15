97517d787ac7e2cd0a3517939e85ad0dadebda2f
Groz <tagir.magomedov@gmail.com>
2014-05-15 16:25:51 -0700

    Removed unnecessary GA links from Regional.fs because everything will be hosted on a single website.

--- FotM.Data/Regional.fs
+++ FotM.Data/Regional.fs
@@ -3,8 +3,6 @@
 type RegionalSettings = {
     code: string;
     blizzardApiRoot: string;
-    googleAnalyticsPropertyCode: string;
-    googleAnalyticsPropertyWebSite: string;
 }
 
 module Regions = 
@@ -13,40 +11,34 @@ module Regions =
 
     let laddersContainer = "ladders"
 
+    let googleAnalyticsPropertyCode = ""
+
+    let googleAnalyticsPropertyWebSite = ""
+
     let US = {
         code = "US"
         blizzardApiRoot = "http://us.battle.net/api/wow/leaderboard/"
-        googleAnalyticsPropertyCode = ""
-        googleAnalyticsPropertyWebSite = ""
     }
 
     let EU = {
         code = "EU"
         blizzardApiRoot = "http://eu.battle.net/api/wow/leaderboard/"
-        googleAnalyticsPropertyCode = ""
-        googleAnalyticsPropertyWebSite = ""
     }
 
     let KR = {
         code = "KR"
         blizzardApiRoot = "http://kr.battle.net/api/wow/leaderboard/"
-        googleAnalyticsPropertyCode = ""
-        googleAnalyticsPropertyWebSite = ""
 
     }
 
     let TW = {
         code = "TW"
         blizzardApiRoot = "http://tw.battle.net/api/wow/leaderboard/"
-        googleAnalyticsPropertyCode = ""
-        googleAnalyticsPropertyWebSite = ""
     }
 
     let CN = {
         code = "CN"
         blizzardApiRoot = "http://www.battlenet.com.cn/api/wow/leaderboard/"
-        googleAnalyticsPropertyCode = ""
-        googleAnalyticsPropertyWebSite = ""
     }
 
     let all = [ US; EU; KR; TW; CN ]
