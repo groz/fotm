7ec6f30765cadf59367d9755e452b2524bbd9af5
Groz <tagir.magomedov@gmail.com>
2014-05-14 17:46:54 -0700

    Added Angular.

---
+++ FotM.Apollo/Scripts/my/main.js
@@ -0,0 +1,11 @@
+angular.module('fotmApp.controllers', []).controller('fotmController',
+    function ($scope) {
+        $scope.list = [
+            1, 2, 3, 4, 5, 6
+        ]
+    }
+);
+
+angular.module('fotmApp', [
+  'fotmApp.controllers'
+]);
