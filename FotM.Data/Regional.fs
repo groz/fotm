742885886101f18cf3358376f81cc33b36292e90
Groz <tagir.magomedov@gmail.com>
2014-05-15 14:57:23 -0700

    Removed unnecessary stuff from RegionalSettings.

--- FotM.Data/Regional.fs
+++ FotM.Data/Regional.fs
@@ -3,16 +3,11 @@
 type RegionalSettings = {
     code: string;
     blizzardApiRoot: string;
-    storageConnection: string;
-    serviceBusConnection: string;
     googleAnalyticsPropertyCode: string;
     googleAnalyticsPropertyWebSite: string;
 }
 
 module Regions = 
-
-    let commonStorage = ""
-    let commonServiceBus = ""
 
     let snapshotsContainer = "snapshots"
 
@@ -21,8 +16,6 @@ module Regions =
     let US = {
         code = "US"
         blizzardApiRoot = "http://us.battle.net/api/wow/leaderboard/"
-        storageConnection = commonStorage
-        serviceBusConnection = commonServiceBus
         googleAnalyticsPropertyCode = ""
         googleAnalyticsPropertyWebSite = ""
     }
@@ -30,18 +23,13 @@ module Regions =
     let EU = {
         code = "EU"
         blizzardApiRoot = "http://eu.battle.net/api/wow/leaderboard/"
-        storageConnection = commonStorage
-        serviceBusConnection = commonServiceBus
         googleAnalyticsPropertyCode = ""
         googleAnalyticsPropertyWebSite = ""
-
     }
 
     let KR = {
         code = "KR"
         blizzardApiRoot = "http://kr.battle.net/api/wow/leaderboard/"
-        storageConnection = commonStorage
-        serviceBusConnection = commonServiceBus
         googleAnalyticsPropertyCode = ""
         googleAnalyticsPropertyWebSite = ""
 
@@ -50,18 +38,13 @@ module Regions =
     let TW = {
         code = "TW"
         blizzardApiRoot = "http://tw.battle.net/api/wow/leaderboard/"
-        storageConnection = commonStorage
-        serviceBusConnection = commonServiceBus
         googleAnalyticsPropertyCode = ""
         googleAnalyticsPropertyWebSite = ""
-
     }
 
     let CN = {
         code = "CN"
         blizzardApiRoot = "http://www.battlenet.com.cn/api/wow/leaderboard/"
-        storageConnection = commonStorage
-        serviceBusConnection = commonServiceBus
         googleAnalyticsPropertyCode = ""
         googleAnalyticsPropertyWebSite = ""
     }
