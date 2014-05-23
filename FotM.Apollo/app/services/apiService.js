e94c5801a66891307519f585ea42ee6f92498dde
Tagir Magomedov <tagir.magomedov@gmail.com>
2014-05-23 01:31:41 -0700

    Caching requests to leaderboard and filtering.

--- FotM.Apollo/app/services/apiService.js
+++ FotM.Apollo/app/services/apiService.js
@@ -1,4 +1,6 @@
-app.factory('api', function($http) {
+app.factory('api', function ($http, $cacheFactory) {
+
+    var lruCache = $cacheFactory('lruCache', { capacity: 50 });
 
     return {
         loadLeaderboardAsync: function(region, bracket, fotmFilters) {
@@ -6,8 +8,11 @@ app.factory('api', function($http) {
             var promise = $http({
                 method: 'GET',
                 url: '/api/' + region + '/' + bracket,
-                params: { filters: fotmFilters }
+                params: { filters: fotmFilters },
+                cache: lruCache
             });
+
+            console.log("cache info:", lruCache.info());
 
             return promise;
         },
