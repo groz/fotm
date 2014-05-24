2d84c256157d94ed6468939e09ed1f4ed0bc8dee
Tagir Magomedov <tagir.magomedov@gmail.com>
2014-05-23 19:56:15 -0700

    Added favico. Initial preparations for Like/Share addition.

--- FotM.Apollo/app/controllers/mainController.js
+++ FotM.Apollo/app/controllers/mainController.js
@@ -1,2 +1,17 @@
-app.controller('MainController', function ($scope) {
+app.controller('MainController', function ($scope, $location) {
+
+    console.log("MainController activated");
+
+    //$scope.mainLink = "http://fotm.info";
+
+    $scope.getCurrentLocation = function () { return $location.absUrl(); }
+
+    $scope.getRoot = function() {
+        var result = $location.protocol() + "://"+$location.host();
+
+        var port = $location.port();
+
+        return (port) ? (result+":"+port) : result;
+    }
+
 });
