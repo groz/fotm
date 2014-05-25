ba4e4d264c30655c14a12c579d749cba7351aa18
Tagir Magomedov <tagir.magomedov@gmail.com>
2014-05-24 23:57:44 -0700

    Added Twitter button. Fixed alignment of social buttons.

--- FotM.Apollo/Controllers/HomeController.fs
+++ FotM.Apollo/Controllers/HomeController.fs
@@ -23,8 +23,8 @@ type HomeController() =
 
     let metaInfo = {
         name = "FotM"
-        title = "Flavor of the Month team ratings for World of Warcraft"
-        description = "Website that shows currently popular team combinations for World of Warcraft arena. It allows for filtering of setups to help finding best one for your team composition and brings the feel of old single-team arenas back."
+        title = "FotM - flavor of the month team rating for World of Warcraft"
+        description = "Website that shows flavor of the month team setups for World of Warcraft arena. Allows for search and filtering of setups to help you find the one that suits you the best and brings the feel of old single-team arenas back."
         rootUrl = root
         previewUrl = root + "/preview.png"
     }
