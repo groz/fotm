48ce31d151141c788f3439e49abc27b9809df60c
Tagir Magomedov <tagir.magomedov@gmail.com>
2014-05-26 04:27:47 -0700

    Updated social settings for VK.

--- FotM.Apollo/Controllers/HomeController.fs
+++ FotM.Apollo/Controllers/HomeController.fs
@@ -19,11 +19,11 @@ type MetaInformation = {
 type HomeController() =
     inherit Controller()
 
-    let root = "http://fotm-info.azurewebsites.net"
+    let root = "http://fotm.info"
 
     let metaInfo = {
         name = "FotM"
-        title = "FotM - flavor of the month team rating for World of Warcraft"
+        title = "FotM - flavor of the month team rankings for World of Warcraft"
         description = "Website that shows flavor of the month team setups for World of Warcraft arena. Allows for search and filtering of setups to help you find the one that suits you the best and brings the feel of old single-team arenas back."
         rootUrl = root
         previewUrl = root + "/preview.png"
